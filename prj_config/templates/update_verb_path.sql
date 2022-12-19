671f0fd7e1eeea1dece3ff2a2798b5dc4b976e3e
vecin2 <vecin2@gmail.com>
2022-12-19 15:22:25 +0100

    Update templates

--- prj_config/templates/update_verb_path.sql
+++ prj_config/templates/update_verb_path.sql
@@ -1,4 +1,5 @@
-{% set __process_descriptor_id = process_descriptor_id | suggest(_keynames.PD) %}
+{% set _process_descriptor_id = process_descriptor_id | suggest(_keynames.PD) %}
+{% set process_descriptor = _db.find.pd_by_keyname(_process_descriptor_id)%}
 UPDATE EVA_PROCESS_DESCRIPTOR
-SET (REPOSITORY_PATH) = ('{{new_process_path | codepath}}')
-WHERE ID = @PD.{{__process_descriptor_id}};
+SET (REPOSITORY_PATH) = ('{{new_process_path | print(process_descriptor) | codepath}}')
+WHERE ID = @PD.{{_process_descriptor_id}};
--- prj_config/templates/update_verb_user_visible_flag.sql
+++
@@ -1,6 +0,0 @@
-{% set _verb_keyname = verb_keyname | suggest(_keynames.V)%}
-{%set verb = _db.find.v_by_keyname(_verb_keyname)%}
-{% set _is_user_visible= is_user_visible | print(verb)%}
-UPDATE EVA_VERB
-set (IS_USER_VISIBLE) = ('{{is_user_visible}}')
-where ID = @V.{{_verb_keyname}}
