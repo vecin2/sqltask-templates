58f6d425a7eb77905c5c4c83e32617c70f9d3930
vecin2 <vecin2@gmail.com>
2023-05-09 15:06:16 +0200

    add suggested property

--- templates/tenant_properties_service/add_tenant_property.sql
+++ templates/tenant_properties_service/add_tenant_property.sql
@@ -1,4 +1,4 @@
-{% set property_names = ["theme.default.logo","theme.default.em-enterprise-desktop.application-title"] %}
+{% set property_names = ["theme.default.logo","theme.default.em-enterprise-desktop.application-title","text.editor.fonts"] %}
 {% set help_message = "Suggested property names are only a few examples. There will be property names that are valid and are not included in the suggested list." %}
 INSERT INTO TENANT_PROPERTY
 (NAME, SHORT_TEXT, LONG_TEXT, LAST_MODIFIED_BY, LAST_MODIFIED_DATE)
--- templates/tenant_properties_service/remove_tenant_property.sql
+++ templates/tenant_properties_service/remove_tenant_property.sql
@@ -1,3 +1,3 @@
-{% set property_names = _tps.fetch.property_names().column('NAME') %}
+{% set property_names = _tps.fetch.all_properties().column('NAME') %}
 DELETE FROM TENANT_PROPERTY
 WHERE name = '{{ name | suggest(property_names) }}';
