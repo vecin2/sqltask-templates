1dacc46ca8320b2582cbb2bd8f149c4d3c075668
vecin2 <vecin2@gmail.com>
2023-10-20 22:44:11 +0200

    fix tests

--- test_templates/tenant_properties_service/test_add_agent_synchronizer_default_mapping.sql
+++ test_templates/tenant_properties_service/test_add_agent_synchronizer_default_mapping.sql
@@ -1,5 +1,5 @@
---{"property":"organisation","value":"organisat"}
+--{"property":"myorganisation","value":"organisat"}
 INSERT INTO TENANT_PROPERTY
 (NAME, SHORT_TEXT, LONG_TEXT, LAST_MODIFIED_BY, LAST_MODIFIED_DATE)
-VALUES('agent-synchronizer.mapping.default.organisation', 'organisat', NULL, 'admin', SYSDATE);
+VALUES('agent-synchronizer.mapping.default.myorganisation', 'organisat', NULL, 'admin', SYSDATE);
 
