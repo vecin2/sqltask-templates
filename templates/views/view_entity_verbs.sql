09dd3643b2f7eaa6f16afa6a801e1f5aa9908548
vecin2 <vecin2@gmail.com>
2023-11-16 11:05:09 +0100

    show extra fields and trim headers

--- templates/views/view_entity_verbs.sql
+++ templates/views/view_entity_verbs.sql
@@ -1,4 +1,4 @@
-SELECT V.ID as verb_ID,id.keyname v_keyname, v.NAME as verb_name, pd.repository_path, V.IS_INSTANCE, V.IS_INSTANCE_DEFAULT as IS_DFLT,v.IS_USER_VISIBLE, pdr.config_id,pdr.config_type_id,pd.config_process_id
+SELECT V.ID as verb_ID,id.keyname v_keyname, v.NAME as verb_name, pd.repository_path, V.IS_INSTANCE as INSTANCE, V.IS_INSTANCE_DEFAULT as IS_DFLT,v.IS_USER_VISIBLE as VISIBLE,v.RECORD_FOR_WRAPUP as rec_wrapup,v.DISPLAY_AT_WRAPUP as disp_wrapup, pdr.config_id,pdr.config_type_id,pd.config_process_id
 FROM EVA_VERB v, EVA_PROCESS_DESC_REFERENCE pdr, EVA_PROCESS_DESCRIPTOR PD,CCADMIN_IDMAP id
 where v.process_desc_ref_id = pdr.id
 AND v.id = id.ID  AND id.KEYSET ='V'
